4f777e567ca0cf55adba4d939ea819a611e31e28
Mahmoud Saad <hodah5948@gmail.com>
2019-04-01 19:58:41 +0200

    first prototype

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var x = document.querySelector('.dropDownList');
+x.style.display = 'none';
+function Display(){
+    var x = document.querySelector('.dropDownList');
+    if (x.style.display === 'none') {
+      x.style.display = 'block';
+    } else {
+      x.style.display = 'none';
+    } 
+}
+
+function displayMap(){
+
+ 
+}
+
