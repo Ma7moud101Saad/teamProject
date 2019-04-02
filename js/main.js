a8fe419aec7c815a9ddac2276482e7baf4d801f9
Mahmoud Saad <hodah5948@gmail.com>
2019-04-02 02:39:01 +0200

    second prototype

--- js/main.js
+++ js/main.js
@@ -8,9 +8,29 @@ function Display(){
       x.style.display = 'none';
     } 
 }
+var modal = document.getElementById('myModel');
+var subModel = document.getElementById('subModel');
+var bigModel = document.getElementById('bigModel');
+var btn = document.getElementById("ShowchoseYouLocation");
 
-function displayMap(){
+var span = document.getElementsByClassName("close")[0];
 
- 
+
+btn.onclick = function() {
+  modal.style.display = "block";
+  subModel.style.display ="block";
+  bigModel.style.display = "none";
+  setTimeout(function(){
+    subModel.style.display = "none";
+    bigModel.style.display = "block";
+  },2000);
+  
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
